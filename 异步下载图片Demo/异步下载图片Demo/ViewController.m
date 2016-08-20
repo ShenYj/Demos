59ba18ea98214ec1422600cf8597966208b8d6bf
ShenYj <shenyanjie123@foxmail.com>
2016-08-20 13:06:47 +0800

    子线程下载刷新UI和子线程下载

--- 异步下载图片Demo/异步下载图片Demo/ViewController.m
+++ 异步下载图片Demo/异步下载图片Demo/ViewController.m
@@ -26,12 +26,32 @@
     
     // 下载图片
 //    [self downLoadImage];  主线程
-    [self downLoadImageBySubThread];// 子线程下载设置图片
+//    [NSThread detachNewThreadSelector:@selector(downLoadImageBySubThread) toTarget:self withObject:nil];// 子线程下载设置图片
+    [NSThread detachNewThreadSelector:@selector(downLoadImageBySubThreadAndBackMainThreadToUpdateUI) toTarget:self withObject:nil];// 子线程下载图片后,回到主线程刷新UI
+}
+
+// 子线程下载,回到主线程来刷新UI
+- (void)downLoadImageBySubThreadAndBackMainThreadToUpdateUI{
+    
+    NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://m.818today.com/imgsy/image/2016/0215/6359115615139200378199199.jpg"]];
+    UIImage *image = [UIImage imageWithData:data];
+    
+    NSLog(@"%@",[NSThread currentThread]);
+    
+    [self performSelectorOnMainThread:@selector(updateUI:) withObject:image waitUntilDone:NO];
+}
+
+- (void)updateUI:(UIImage *)image{
+    
+    NSLog(@"%@",[NSThread currentThread]);
+    self.imageView.image = image;
 }
 
 // 子线程下载
 - (void)downLoadImageBySubThread{
     
+    
+    NSLog(@"%@",[NSThread currentThread]);
     NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://m.818today.com/imgsy/image/2016/0215/6359115615139200378199199.jpg"]];
     UIImage *image = [UIImage imageWithData:data];
     self.imageView.image = image;
