1666810e0907122cba3a204f9c488ed0b0b04cd6
ShenYj <shenyanjie123@foxmail.com>
2016-09-18 21:52:34 +0800

    OC与Swift类相互调用Demo

---
+++ OC_Swift/OC_Swift/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  OC_Swift
+//
+//  Created by ShenYj on 16/9/18.
+//  Copyright © 2016年 ___ShenYJ___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ OC_Swift/OC_Swift/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  OC_Swift
+//
+//  Created by ShenYj on 16/9/18.
+//  Copyright © 2016年 ___ShenYJ___. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
