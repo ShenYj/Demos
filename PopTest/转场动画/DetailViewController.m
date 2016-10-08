346dd68e9da6ae9eed49e32d7b9ad48b2b05bab8
ShenYj <shenyanjie123@foxmail.com>
2016-10-08 13:00:38 +0800

    转场动画 Type moveIn

--- PopTest/转场动画/DetailViewController.m
+++ PopTest/转场动画/DetailViewController.m
@@ -34,7 +34,7 @@
     CATransition *transition = [CATransition animation];
     
     // 2.设置类型
-    transition.type = @"push";
+    transition.type = @"moveIn";
     
     if (gesture.direction == UISwipeGestureRecognizerDirectionLeft) {
         
