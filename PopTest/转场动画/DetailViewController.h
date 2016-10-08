6985abc40656315961e062e852a45429251250b7
ShenYj <shenyanjie123@foxmail.com>
2016-10-08 12:33:31 +0800

    转场动画

---
+++ PopTest/转场动画/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  转场动画
+//
+//  Created by ShenYj on 2016/10/8.
+//  Copyright © 2016年 ShenYj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
