7bed081d9cbdb575c19f40595af1964e0532d887
ShenYj <shenyanjie123@foxmail.com>
2017-01-22 10:37:58 +0800

    基本UI

---
+++ StackView练习/StackView练习/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  StackView练习
+//
+//  Created by ShenYj on 2017/1/22.
+//  Copyright © 2017年 ShenYj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
