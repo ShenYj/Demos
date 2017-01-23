4303413f0213ce3d3447936eda0c86923349a8e6
ShenYj <shenyanjie123@foxmail.com>
2017-01-23 21:23:24 +0800

    CALayer 的mask属性使用

---
+++ CALayer的mask属性/CALayer的mask属性/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CALayer的mask属性
+//
+//  Created by ShenYj on 2017/1/23.
+//  Copyright © 2017年 ShenYj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CALayer的mask属性/CALayer的mask属性/ViewController.m
@@ -0,0 +1,46 @@
+//
+//  ViewController.m
+//  CALayer的mask属性
+//
+//  Created by ShenYj on 2017/1/23.
+//  Copyright © 2017年 ShenYj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (nonatomic) UIImageView *imageView;
+@property (nonatomic) CALayer *maskLayer;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"IMG_3946"]];
+    self.imageView.frame = CGRectMake(0, 100, [UIScreen mainScreen].bounds.size.width, 300);
+    self.imageView.contentMode = UIViewContentModeScaleAspectFit;
+    self.imageView.layer.borderWidth = 1.0;
+    self.imageView.layer.borderColor = [UIColor blackColor].CGColor;
+    [self.view addSubview:self.imageView];
+    
+    
+    
+    self.maskLayer = [CALayer layer];
+    self.maskLayer.contents = (__bridge id _Nullable)([UIImage imageNamed:@"arrow"].CGImage);
+    self.maskLayer.frame = self.imageView.bounds;
+    
+    self.imageView.layer.mask = self.maskLayer;
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
