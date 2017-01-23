08479aff9c330e27a8bb2891a3c6e2af9ad23aa5
ShenYj <shenyanjie123@foxmail.com>
2017-01-23 21:27:12 +0800

    CALayer 的shadow属性效果

--- CALayer的mask属性/CALayer的mask属性/ViewController.m
+++ CALayer的mask属性/CALayer的mask属性/ViewController.m
@@ -21,21 +21,27 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"IMG_3946"]];
-    self.imageView.frame = CGRectMake(0, 100, [UIScreen mainScreen].bounds.size.width, 300);
+    self.imageView.frame = CGRectMake(20, 100, [UIScreen mainScreen].bounds.size.width - 40, 300);
     self.imageView.contentMode = UIViewContentModeScaleAspectFit;
     self.imageView.layer.borderWidth = 1.0;
     self.imageView.layer.borderColor = [UIColor blackColor].CGColor;
     [self.view addSubview:self.imageView];
     
+    self.imageView.layer.shadowOpacity = 0.5;
+    self.imageView.layer.shadowOffset = CGSizeMake(-10, 10);
+    self.imageView.layer.shadowRadius = 5.0;
     
+    //self.imageView.layer.masksToBounds = YES; 会裁切子视图和阴影
     
+    /*
     self.maskLayer = [CALayer layer];
     self.maskLayer.contents = (__bridge id _Nullable)([UIImage imageNamed:@"arrow"].CGImage);
     self.maskLayer.frame = self.imageView.bounds;
     
     self.imageView.layer.mask = self.maskLayer;
+     */
+    
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
