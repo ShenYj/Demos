50fa1e5873ece08e0a4de4015c76d11c63aa8f4b
ShenYj <shenyanjie123@foxmail.com>
2017-02-11 10:19:27 +0800

    sublayerTransform

---
+++ transform/sublayerTransform/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sublayerTransform
+//
+//  Created by ShenYj on 2017/2/11.
+//  Copyright © 2017年 ShenYj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ transform/sublayerTransform/ViewController.m
@@ -0,0 +1,43 @@
+//
+//  ViewController.m
+//  sublayerTransform
+//
+//  Created by ShenYj on 2017/2/11.
+//  Copyright © 2017年 ShenYj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UIView *containerView;
+@property (weak, nonatomic) IBOutlet UIImageView *leftView;
+@property (weak, nonatomic) IBOutlet UIImageView *rightView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+
+    CATransform3D perspective = CATransform3DIdentity;
+    perspective.m34 = - 1.0 / 500.0;
+    self.containerView.layer.sublayerTransform = perspective;
+    //rotate layerView1 by 45 degrees along the Y axis
+    CATransform3D transform1 = CATransform3DMakeRotation(M_PI_4, 0, 1, 0);
+    self.leftView.layer.transform = transform1;
+    //rotate layerView2 by 45 degrees along the Y axis
+    CATransform3D transform2 = CATransform3DMakeRotation(-M_PI_4, 0, 1, 0);
+    self.rightView.layer.transform = transform2;
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
