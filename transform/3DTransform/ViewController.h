5b5f1ad9f728d7ec4672dbaf5649bb5faa5688ee
ShenYj <shenyanjie123@foxmail.com>
2017-02-11 10:08:24 +0800

    m34

---
+++ transform/3DTransform/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  3DTransform
+//
+//  Created by ShenYj on 2017/2/10.
+//  Copyright © 2017年 ShenYj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ transform/3DTransform/ViewController.m
@@ -0,0 +1,51 @@
+//
+//  ViewController.m
+//  3DTransform
+//
+//  Created by ShenYj on 2017/2/10.
+//  Copyright © 2017年 ShenYj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property(nonatomic) UIView *redView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.redView = [[UIView alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
+    self.redView.backgroundColor = [UIColor redColor];
+    [self.view addSubview:self.redView];
+    
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    
+    //self.redView.layer.transform = CATransform3DRotate(self.redView.layer.transform, M_PI_4, 1, 1, 1);
+    
+    
+    //create a new transform
+    CATransform3D transform = CATransform3DIdentity;
+    //apply perspective
+    transform.m34 = - 1.0 / 500.0;
+    //rotate by 45 degrees along the Y axis
+    transform = CATransform3DRotate(transform, M_PI_4, 0, 1, 0);
+    //apply to layer
+    self.redView.layer.transform = transform;
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
