997919c3ef2425f85099d8d37ada8afa1a86afcc
ShenYj <shenyanjie123@foxmail.com>
2017-02-12 10:32:56 +0800

    core text

--- transform/CATextLayer/ViewController.m
+++ transform/CATextLayer/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import <QuartzCore/QuartzCore.h>
+#import <CoreText/CoreText.h>
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UIView *labelView;
@@ -18,6 +20,57 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self demo2];
+}
+
+- (void)demo2 {
+    //create a text layer
+    CATextLayer *textLayer = [CATextLayer layer];
+    textLayer.frame = self.labelView.bounds;
+    textLayer.contentsScale = [UIScreen mainScreen].scale;
+    [self.labelView.layer addSublayer:textLayer];
+    
+    //set text attributes
+    textLayer.alignmentMode = kCAAlignmentJustified;
+    textLayer.wrapped = YES;
+    
+    //choose a font
+    UIFont *font = [UIFont systemFontOfSize:15];
+    
+    //choose some text
+    NSString *text = @"Lorem ipsum dolor sit amet, consectetur adipiscing \ elit. Quisque massa arcu, eleifend vel varius in, facilisis pulvinar \ leo. Nunc quis nunc at mauris pharetra condimentum ut ac neque. Nunc \ elementum, libero ut porttitor dictum, diam odio congue lacus, vel \ fringilla sapien diam at purus. Etiam suscipit pretium nunc sit amet \ lobortis";
+    
+    //create attributed string
+    NSMutableAttributedString *string = nil;
+    string = [[NSMutableAttributedString alloc] initWithString:text];
+    
+    //convert UIFont to a CTFont
+    CFStringRef fontName = (__bridge CFStringRef)font.fontName;
+    CGFloat fontSize = font.pointSize;
+    CTFontRef fontRef = CTFontCreateWithName(fontName, fontSize, NULL);
+    
+    //set text attributes
+    NSDictionary *attribs = @{
+                              (__bridge id)kCTForegroundColorAttributeName:(__bridge id)[UIColor blackColor].CGColor,
+                              (__bridge id)kCTFontAttributeName: (__bridge id)fontRef
+                              };
+    
+    [string setAttributes:attribs range:NSMakeRange(0, [text length])];
+    attribs = @{
+                (__bridge id)kCTForegroundColorAttributeName: (__bridge id)[UIColor redColor].CGColor,
+                (__bridge id)kCTUnderlineStyleAttributeName: @(kCTUnderlineStyleSingle),
+                (__bridge id)kCTFontAttributeName: (__bridge id)fontRef
+                };
+    [string setAttributes:attribs range:NSMakeRange(6, 5)];
+    
+    //release the CTFont we created earlier
+    CFRelease(fontRef);
+    
+    //set layer text
+    textLayer.string = string;
+}
+
+- (void)demo1{
     //create a text layer
     CATextLayer *textLayer = [CATextLayer layer];
     textLayer.frame = self.labelView.bounds;
