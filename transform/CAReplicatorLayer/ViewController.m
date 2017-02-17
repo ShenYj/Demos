2614bd22ee48cd278681099f0cacb22d0e214562
ShenYj <shenyanjie123@foxmail.com>
2017-02-17 10:03:56 +0800

    CAReplicatorLayer

--- transform/CAReplicatorLayer/ViewController.m
+++ transform/CAReplicatorLayer/ViewController.m
@@ -10,6 +10,8 @@
 
 @interface ViewController ()
 
+@property (nonatomic) UIView *containerView;
+
 @end
 
 @implementation ViewController
@@ -17,6 +19,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.containerView = [[UIView alloc] initWithFrame:CGRectMake(200, 100, 100, 100)];
+    self.containerView.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:self.containerView];
+    
+    
+    CAReplicatorLayer *replicator = [CAReplicatorLayer layer];
+    replicator.frame = self.containerView.bounds;
+    [self.containerView.layer addSublayer:replicator];
+    
+    //configure the replicator
+    replicator.instanceCount = 10;
+    
+    //apply a transform for each instance
+    CATransform3D transform = CATransform3DIdentity;
+    transform = CATransform3DTranslate(transform, 0, 200, 0);
+    transform = CATransform3DRotate(transform, M_PI / 5.0, 0, 0, 1);
+    transform = CATransform3DTranslate(transform, 0, -200, 0);
+    replicator.instanceTransform = transform;
+    
+    //apply a color shift for each instance
+    replicator.instanceBlueOffset = -0.1;
+    replicator.instanceGreenOffset = -0.1;
+    
+    //create a sublayer and place it inside the replicator
+    CALayer *layer = [CALayer layer];
+    layer.frame = CGRectMake(100.0f, 100.0f, 100.0f, 100.0f);
+    layer.backgroundColor = [UIColor whiteColor].CGColor;
+    [replicator addSublayer:layer];
 }
 
 
