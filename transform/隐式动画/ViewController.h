4d322896526938f004791241db7f18a84d921146
ShenYj <shenyanjie123@foxmail.com>
2017-02-19 22:20:35 +0800

    隐式动画

---
+++ transform/隐式动画/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  隐式动画
+//
+//  Created by ShenYj on 2017/2/19.
+//  Copyright © 2017年 ShenYj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ transform/隐式动画/ViewController.m
@@ -0,0 +1,60 @@
+//
+//  ViewController.m
+//  隐式动画
+//
+//  Created by ShenYj on 2017/2/19.
+//  Copyright © 2017年 ShenYj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (nonatomic, weak) IBOutlet UIView *layerView;
+@property (nonatomic, strong) CALayer *colorLayer;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    //create sublayer
+    self.colorLayer = [CALayer layer];
+    self.colorLayer.frame = CGRectMake(85.0f, 50.0f, 100.0f, 100.0f);
+    self.colorLayer.backgroundColor = [UIColor blueColor].CGColor;
+    //add it to our view
+    [self.layerView.layer addSublayer:self.colorLayer];
+    
+}
+- (IBAction)changeColor {
+    //begin a new transaction
+    [CATransaction begin];
+    //set the animation duration to 1 second
+    [CATransaction setAnimationDuration:1.0];
+    // turn off animation
+    //[CATransaction setDisableActions:YES];
+    //add the spin animation on completion
+    [CATransaction setCompletionBlock:^{
+        //rotate the layer 90 degrees
+        CGAffineTransform transform = self.colorLayer.affineTransform;
+        transform = CGAffineTransformRotate(transform, M_PI_2);
+        self.colorLayer.affineTransform = transform;
+    }];
+    //randomize the layer background color
+    CGFloat red = arc4random() / (CGFloat)INT_MAX;
+    CGFloat green = arc4random() / (CGFloat)INT_MAX;
+    CGFloat blue = arc4random() / (CGFloat)INT_MAX;
+    self.colorLayer.backgroundColor = [UIColor colorWithRed:red green:green blue:blue alpha:1.0].CGColor;
+    //commit the transaction
+    [CATransaction commit];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
