466617c1feb63c968f0e4a66ed26d051ca8d260d
ShenYj <shenyanjie123@foxmail.com>
2017-03-01 20:34:02 +0800

    图层行为

--- transform/图层行为/ViewController.m
+++ transform/图层行为/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+
 @property (weak, nonatomic) IBOutlet UIView *layerView;
 
 @end
@@ -20,10 +21,23 @@
     // Do any additional setup after loading the view, typically from a nib.
     //set the color of our layerView backing layer directly
     self.layerView.layer.backgroundColor = [UIColor blueColor].CGColor;
-    dispatch_queue_create()
+
+    /** 
+     当属性在动画块之外发生改变，UIView直接通过返回nil来禁用隐式动画。但如果在动画块范围之内，根据动画具体类型返回相应的属性，在这个例子就是CABasicAnimation
+     */
+    //test layer action when outside of animation block
+    NSLog(@"Outside: %@", [self.layerView actionForLayer:self.layerView.layer forKey:@"backgroundColor"]);
+    //begin animation block
+    [UIView beginAnimations:nil context:nil];
+    //test layer action when inside of animation block
+    NSLog(@"Inside: %@", [self.layerView actionForLayer:self.layerView.layer forKey:@"backgroundColor"]);
+    //end animation block
+    [UIView commitAnimations];
+    
 }
 
 - (IBAction)changeColor {
+    
     //begin a new transaction
     [CATransaction begin];
     //set the animation duration to 1 second
