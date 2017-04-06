ec124f83944decc16441bdb30f8f5bc15e6b89a8
ShenYj <shenyj4@51nb.com>
2017-04-06 17:16:04 +0800

    蓝牙工具类-搜索设备封装

---
+++ CorebluetoothManager/CorebluetoothManager/demoViewController.h
@@ -0,0 +1,15 @@
+//
+//  demoViewController.h
+//  蓝牙(CoreBluetooth)
+//
+//  Created by ecg on 2017/4/6.
+//  Copyright © 2017年 ShenYj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+
+@interface demoViewController : UIViewController
+
+@end
